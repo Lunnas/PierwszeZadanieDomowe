d1c87a0579ea6b4c16793544268d99dd8b17df01
LAPTOP-AQKISACC\ewawa <ewawar01484@gmail.com>
2021-08-05 22:19:05 +0200

    Poprawione zadanie

--- src/wczytywanie/ZadanieDomowe1.java
+++ src/wczytywanie/ZadanieDomowe1.java
@@ -1,33 +1,28 @@
 package wczytywanie;
 import java.util.Scanner;
 
-public class MetodaMain {
+public class ZadanieDomowe1 {
 
     public static void main(String[] args) {
-       // int wiek;
-        //if (wiek < 0) return;
-       // if (wiek > 150) return;
+
         Scanner in = new Scanner(System.in);
 
         System.out.println("Podaj swój wiek:");
         int wiek = in.nextInt();
-
-        System.out.println("Twój wiek to: " + wiek);
         if (wiek < 0) return;
         if (wiek > 150) return;
 
-        if (wiek <= 17) {
+        System.out.println("Twój wiek to: " + wiek);
+
+        if (wiek < 18) {
             System.out.println("Jesteś dzieckiem");
-        }
-        if (wiek >=18 && wiek <= 39) {
+        } else if (wiek >=18 && wiek < 40) {
             System.out.println("Jesteś dorosłym");
 
-        }
-        if (wiek >=40 && wiek <= 59){
+        } else if (wiek >=40 && wiek < 60) {
             System.out.println("Jesteś w średnim wieku");
 
-        }
-        if (wiek>=60) {
+        } else {
             System.out.println("Jesteś w podeszłym wieku");
         }
     }
